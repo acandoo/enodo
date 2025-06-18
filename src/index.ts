abe76372ffa581620135857aaca3f9186d93539d
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-18 00:47:29 -0400

    refactor: replace test script with git clone functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-(async () => {
-    console.log("test");
-})();
+#!/bin/env node
+
+import git from "isomorphic-git";
+import http from "isomorphic-git/http/node";
+import fs from "fs";
+
+await git.clone({
+    fs,
+    http,
+    dir: "./repo", // local directory to clone into
+    url: "https://github.com/octocat/Hello-World", // repository URL
+});
