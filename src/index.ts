6674eacb7798967206196b3f2b5dce1c868d32ab
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-19 00:41:45 -0400

    feat: enhance cloning process with progress logging and author tracking

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import git from 'isomorphic-git'
 import http from 'isomorphic-git/http/node/index.js'
 import fs from 'node:fs'
 
-const URL = 'https://github.com/octocat/Hello-World'
-const DIRECTORY = process.argv[2] || './repo'
+const URL = process.argv[2] || 'https://github.com/octocat/Hello-World'
+const DIRECTORY = process.argv[3] || './repo'
+
+type Author = {
+    name: string
+    email: string
+    commits: number
+}
+
+const authors: Author[] = []
 
 await git.clone({
     fs,
     http,
-    dir: DIRECTORY, // local directory to clone into
-    url: URL, // repository URL
+    dir: DIRECTORY,
+    url: URL,
+    onProgress: (event) => {
+        if (event.loaded) {
+            // I have no clue why but the total is double the actual size
+            console.log(
+                `${event.phase}: ${Math.round((event.loaded / event.total) * 50)}%`
+            )
+        }
+    },
+    onMessage: (message) => console.log(`Message: ${message}`),
+    singleBranch: true,
 })
 
 const results = await git.log({
@@ -19,4 +37,22 @@ const results = await git.log({
     dir: DIRECTORY,
 })
 
-console.log(results)
+console.log('Cloned!')
+
+results
+    .map((entry) => entry.commit.author)
+    .forEach((author) => {
+        // Check if author already exists in authors array
+        const existingAuthor = authors.find(
+            (a) => a.name === author.name && a.email === author.email
+        )
+        if (existingAuthor) {
+            existingAuthor.commits += 1
+        } else {
+            authors.push({ name: author.name, email: author.email, commits: 1 })
+        }
+    })
+
+authors.sort((a, b) => b.commits - a.commits)
+
+console.log(authors)
