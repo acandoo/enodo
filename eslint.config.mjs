51aa194a9dee77426608140045182527f13facf8
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-19 00:48:17 -0400

    refactor: follow .gitignore for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,13 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 export default defineConfig([
+    includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         plugins: { js },
