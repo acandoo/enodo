a8af39ad1f695ac73031ea2d95c6a113882672e7
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-18 23:39:46 -0400

    feat: log results to console

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 #!/bin/env node
 
-import git from "isomorphic-git";
-import http from "isomorphic-git/http/node/index.js";
-import fs from "fs";
+import git from 'isomorphic-git'
+import http from 'isomorphic-git/http/node/index.js'
+import fs from 'fs'
+
+const URL = 'https://github.com/octocat/Hello-World'
+const DIRECTORY = process.argv[2] || './repo'
 
 await git.clone({
     fs,
     http,
-    dir: "./repo", // local directory to clone into
-    url: "https://github.com/octocat/Hello-World", // repository URL
-});
+    dir: DIRECTORY, // local directory to clone into
+    url: URL, // repository URL
+})
+
+const results = await git.log({
+    fs,
+    dir: DIRECTORY,
+})
+
+console.log(results)
